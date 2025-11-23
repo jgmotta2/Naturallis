a5e73fbc9eb1c5747ea242ba4bac15e260fc6a72
Jgmotta2 <jgmotta2004@gmail.com>
2025-11-23 10:47:17 -0300

    feat(api): enhance request interceptor to check for public routes before adding authorization token

--- services/api.ts
+++ services/api.ts
@@ -14,9 +14,16 @@ const api = axios.create({
 api.interceptors.request.use(
   async (config) => {
     try {
-      const token = await SecureStore.getItemAsync("user_token");
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
+      const publicRoutes = ["/auth/signup", "/auth/signin", "/auth/login"];
+
+      const isPublicRoute =
+        config.url && publicRoutes.some((route) => config.url?.includes(route));
+
+      if (!isPublicRoute) {
+        const token = await SecureStore.getItemAsync("user_token");
+        if (token) {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
       }
     } catch (error) {
       console.error("Erro ao recuperar token", error);
@@ -32,13 +39,15 @@ api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response) {
-      console.log("❌ Erro na resposta:", {
+      console.log("Erro na resposta:", {
         status: error.response.status,
         data: error.response.data,
         url: error.config.url,
       });
     } else if (error.request) {
-      console.log("❌ Sem resposta do servidor:", error.message);
+      console.log("Sem resposta do servidor (Network Error):", error.message);
+    } else {
+      console.log("Erro na configuração da requisição:", error.message);
     }
     return Promise.reject(error);
   }
